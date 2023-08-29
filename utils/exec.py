559b7cc8f7f9524d5b90b447deb0da9209a29c98
vladik-activefence <71721322+vladik-activefence@users.noreply.github.com>
2023-08-29 13:20:02 +0300

    Update exec.py
    Drop the docopt and use argparse

--- utils/exec.py
+++ utils/exec.py
@@ -1,34 +1,23 @@
 #!/usr/bin/env python
-
 from __future__ import unicode_literals
-
-"""Exec Smali Files.
-
-Usage:
-    exec.py -i File.smali -m methodName [-p methodParameters]
-
-Options:
-    -h --help        Show this screen.
-    -i <filename>    The smali file to execute.
-    -m <method>      The name of the method to execute.
-    -p <parameters>  A list of parameters to give as arguments.
-                     If not provided, the script will introspect the method
-                     and give insights about what parameters are expected.
-"""
-
-from docopt import docopt
+import argparse
 import smali.emulator
+import smali.classloader
 import ast
 
 
-def main(arguments):
-    filename = arguments.get('-i')
-    parameters = arguments.get('-p')
+def main(filename, methodName, parameters):
     parameters = ast.literal_eval(parameters) if parameters else {}
-    emu = smali.emulator.Emulator()
+    cl = smali.classloader.ClassLoader()
+    emu = smali.emulator.Emulator(class_loader=cl)
     result = emu.run_file(filename, parameters)
     print(result)
 
-
 if __name__ == '__main__':
-    main(docopt(__doc__))
+    parser = argparse.ArgumentParser(prog='exec.py', description='fill')
+    parser.add_argument("-i", help="Input SMALI file")
+    parser.add_argument("-m", help="Method name", required=True)
+    parser.add_argument("-p", help="Parameters of the method", required=False)
+    args = parser.parse_args()
+    inputFile, methodName, parameters = args.i, args.m, args.p
+    main(inputFile, methodName, parameters)
